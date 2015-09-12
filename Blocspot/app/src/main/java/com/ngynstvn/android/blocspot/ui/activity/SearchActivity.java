c79c654cf77ceb8e765da1401a1dd881d2ba904e
Steven Nguyen <ngynstvn@gmail.com>
2015-09-11 18:50:46 -0700

    Got access to JSON data from YelpAPI instantiation and parse data to get necessary information. Resorted to http://jsonlint.com/ to get organized JSON data. (made life so much easier)
    Omg...breakthrough of the day!

--- Blocspot/app/src/main/java/com/ngynstvn/android/blocspot/ui/activity/SearchActivity.java
+++ Blocspot/app/src/main/java/com/ngynstvn/android/blocspot/ui/activity/SearchActivity.java
@@ -5,21 +5,42 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.ngynstvn.android.blocspot.BlocspotApplication;
 import com.ngynstvn.android.blocspot.R;
+import com.ngynstvn.android.blocspot.api.yelp.YelpAPI;
 import com.ngynstvn.android.blocspot.ui.adapter.SearchAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class SearchActivity extends Activity {
+
+    private static final String TAG = "Test (" + SearchActivity.class.getSimpleName() + ")";
 
     private RecyclerView recyclerView;
     private SearchAdapter searchAdapter;
     private SQLiteDatabase database = BlocspotApplication.getSharedDataSource().getDatabaseOpenHelper().getReadableDatabase();
     private Cursor cursor;
+
+    private ArrayList<String> resultList = new ArrayList<>();
+
+    private static final String CONSUMER_KEY = "Y0Myyxe8h_YEsHjfo-j6yg";
+    private static final String CONSUMER_SECRET = "1qMgDlgy15dRMjh5ImZqjtEAXRg";
+    private static final String TOKEN = "6gp-qC2WHzIw6DKpTu-725G73pXmIxXx";
+    private static final String TOKEN_SECRET = "H7yDe6WlFTMvSYcEi-TIDE0CUZk";
+
+    private YelpAPI yelpAPI;
+    private String jsonResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,33 +51,71 @@ public class SearchActivity extends Activity {
 
         if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            showResults(query);
+            runSearch(query, "Los Angeles, CA");
         }
+
     }
 
-    private void showResults(String query) {
+    private void runSearch(final String term, final String location) {
 
-        String[] split = query.split(" ");
-        String queryMessage = null;
+        new AsyncTask<Void, Void, String>() {
 
-        if(split.length == 1) {
-            queryMessage = "Select * from poi_table where location_name like '%" + split[0] +"%'";
-        }
-        else if(split.length == 2) {
-            queryMessage = "Select * from poi_table where location_name like '%" + split[0] +"%' or " +
-                    "location_name like '" + split[1] +"'";
-        }
-        else {
-            queryMessage = "Select * from poi_table where location_name like '%" + split[0] +"%' or " +
-                    "location_name like '%" + split[1] +"%' or location_name like '%" + split[2] + "%'";
-        }
+            @Override
+            protected void onPreExecute() {
+                yelpAPI = YelpAPI.newInstance();
+            }
 
-        cursor = database.rawQuery(queryMessage, null);
+            @Override
+            protected String doInBackground(Void... params) {
+                return yelpAPI.searchForBusinessesByLocation(term, location);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                Log.v(TAG, s);
+                getResults(s);
+            }
+
+        }.execute();
+
+    }
+
+    private void getResults(String jsonString) {
+
+        parseJSONstring(jsonString);
+
         searchAdapter = new SearchAdapter(BlocspotApplication.getSharedInstance(), cursor);
         recyclerView = (RecyclerView) findViewById(R.id.rv_activity_search);
         recyclerView.setLayoutManager(new LinearLayoutManager(BlocspotApplication.getSharedInstance()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(searchAdapter);
+    }
+
+    private void parseJSONstring(String jsonString) {
+
+        // Parse the JSON string here
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray("businesses");
+
+            for(int i = 0; i < jsonArray.length(); i++) {
+                String location_name = jsonArray.optJSONObject(i).getString("name");
+                String address = jsonArray.optJSONObject(i).optJSONObject("location").getJSONArray("address").getString(0);
+                String city = jsonArray.optJSONObject(i).optJSONObject("location").getString("city");
+                String state = jsonArray.optJSONObject(i).optJSONObject("location").getString("state_code");
+                double latitude = jsonArray.optJSONObject(i).optJSONObject("location").optJSONObject("coordinate").getDouble("latitude");
+                double longitude = jsonArray.optJSONObject(i).optJSONObject("location").optJSONObject("coordinate").getDouble("longitude");
+
+                Log.v(TAG, "Place #" + i + ": " + location_name + " | " + address + " | " + city + " | " + state + " | " + latitude + " | " + longitude);
+            }
+
+            Log.v(TAG, "Current jsonArray size = " + jsonArray.length());
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
